5dbe98871cf1227e3c19ec9813cd450e5b163568
飯野　健二 <yakamoto@mac.com>
2020-01-23 23:07:02 +0900

    一旦セーブ

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,18 @@
-import kotlin.math.min
-import kotlin.math.max
+import java.io.BufferedReader
+import java.io.InputStream
+import java.io.InputStreamReader
+import java.util.*
 import kotlin.math.abs
+import kotlin.math.max
+import kotlin.math.min
 
 val MOD = 1_000_000_007
 
-fun main() {
-  val (N) = readInts()
-  println(N)
-}
+class Solver(stream: InputStream, private val out: java.io.PrintWriter) {
+  fun solve() {
+    val N = ni()
+    out.println(N)
+  }
 
 
 
@@ -52,33 +57,77 @@ fun main() {
 
 
 
-private val isDebug = try {
-  // なんか本番でエラーでる
-  System.getenv("MY_DEBUG") != null
-} catch (t: Throwable) {
-  false
-}
 
-private fun readLn() = readLine()!!
-private fun readStrings() = readLn().split(" ")
-private fun readInts() = readStrings().map { it.toInt() }.toIntArray()
-private fun readLongs() = readStrings().map { it.toLong() }.toLongArray()
-private inline fun debug(msg: () -> String) {
-  if (isDebug) System.err.println(msg())
-}
-private fun debug(a: LongArray) {
-  debug{a.joinToString(" ")}
-}
-private fun debug(a: IntArray) {
-  debug{a.joinToString(" ")}
-}
-private fun debug(a: BooleanArray) {
-  debug{a.map{if(it) 1 else 0}.joinToString("")}
-}
-private fun debugDim(A: Array<IntArray>) {
-  if (isDebug) {
-    for (a in A) {
-      debug(a)
+  private val isDebug = try {
+    // なんか本番でエラーでる
+    System.getenv("MY_DEBUG") != null
+  } catch (t: Throwable) {
+    false
+  }
+
+  private var tokenizer: StringTokenizer? = null
+  private val reader = BufferedReader(InputStreamReader(stream), 32768)
+  private fun next(): String {
+    while (tokenizer == null || !tokenizer!!.hasMoreTokens()) {
+      tokenizer = StringTokenizer(reader.readLine())
+    }
+    return tokenizer!!.nextToken()
+  }
+
+  private fun ni() = next().toInt()
+  private fun nl() = next().toLong()
+  private fun ns() = next()
+  private fun na(n: Int, offset: Int = 0): IntArray {
+    return map(n, offset) { ni() }
+  }
+
+  private inline fun map(n: Int, offset: Int = 0, f: (Int) -> Int): IntArray {
+    val res = IntArray(n)
+    for (i in 0 until n) {
+      res[i] = f(i + offset)
+    }
+    return res
+  }
+
+  private inline fun debug(msg: () -> String) {
+    if (isDebug) System.err.println(msg())
+  }
+
+  private fun debug(a: LongArray) {
+    debug { a.joinToString(" ") }
+  }
+
+  private fun debug(a: IntArray) {
+    debug { a.joinToString(" ") }
+  }
+
+  private fun debug(a: BooleanArray) {
+    debug { a.map { if (it) 1 else 0 }.joinToString("") }
+  }
+
+  private fun debugDim(A: Array<IntArray>) {
+    if (isDebug) {
+      for (a in A) {
+        debug(a)
+      }
     }
   }
+
+  /**
+   * 勝手にimport消されるのを防ぎたい
+   */
+  private fun hoge() {
+    min(1, 2)
+    max(1, 2)
+    abs(-10)
+  }
+}
+
+fun <A, B> pair(a: A, b: B) = RPair(a, b)
+data class RPair<A, B>(val _1: A, val _2: B)
+
+fun main() {
+  val out = java.io.PrintWriter(System.out)
+  Solver(System.`in`, out).solve()
+  out.flush()
 }
---
+++ src/test/kotlin/lib/DelayMergeTree.kt
@@ -0,0 +1,79 @@
+package lib
+
+import kotlin.math.min
+
+class DelayMergeTree(n: Int) {
+  private val N =
+    if (Integer.highestOneBit(n) == n) n
+    else Integer.highestOneBit(n) shl 1
+
+  private val value = IntArray(N * 2)
+  private val delay = IntArray(N * 2)
+
+  private fun push(k: Int) {
+    if (k < N) {
+      value[k * 2] += delay[k]
+      value[k * 2 + 1] += delay[k]
+      delay[k * 2] += delay[k]
+      delay[k * 2 + 1] += delay[k]
+      delay[k] = 0
+    }
+  }
+
+  // ここをmax, minにする
+  private inline fun merge(a: Int, b: Int): Int = min(a, b)
+
+  // 条件を満たすか。maxなら '>' minなら '<' が使える
+  private inline fun contains(a: Int, x: Int): Boolean = a < x
+
+  /**
+   * [a, b)
+   */
+  fun add(a: Int, b: Int, x: Int, k: Int = 1, l: Int = 0, r: Int = N) {
+    if (a >= r || l >= b) return // ノードが範囲からはずれてる
+    if (a <= l && r <= b) { // ノードが完全に範囲に含まれる
+      value[k] += x
+      delay[k] += x
+      return
+    }
+
+    push(k)
+    val m = (l + r) / 2
+    val lft = k * 2
+    val rgt = lft + 1
+    add(a, b, x, lft, l, m)
+    add(a, b, x, rgt, m, r)
+    value[k] = merge(value[lft], value[rgt])
+  }
+
+  /**
+   * @param x 左右どちらからか最初に a > x になる場所を探す
+   * @return -1
+   */
+  fun query(x: Int, k: Int = 1, l: Int = 0, r: Int = N): Int {
+    if (!contains(value[k], x)) return -1
+    if (l + 1 == r) return r // 1-indexed
+
+    push(k)
+    val m = (l + r) / 2
+    val lft = k * 2
+    val rgt = lft + 1
+
+    // 右から探す。左から探したい場合は左右を逆にすればいい
+    val ans = query(x, rgt, m, r)
+    if (ans != -1) return ans
+    return query(x, lft, l, m)
+  }
+
+  fun eval(i: Int): Int {
+    _eval(N + i)
+    return value[N + i]
+  }
+
+  fun _eval(k: Int) {
+    if (k > 1) {
+      _eval(k / 2)
+    }
+    push(k)
+  }
+}
